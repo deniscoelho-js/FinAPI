e0d41b8466066d028b168650054fe6635ded1748
Denis Coelho <deniscoelho.dev@gmail.com>
2021-12-07 09:46:28 -0300

    atualizacao

---
+++ src/index.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const {v4: uuidv4 } = require('uuid')
+
+const app = express()
+
+app.use(express.json())
+
+const customers = []
+
+function verifyIfExistsAccountCPF(req, res, next){
+    const { cpf } = req.headers
+
+    const customer = customers.find((customer)=> customer.cpf === cpf)
+
+    if(!customer){
+        return res.status(400).json({error: "Customer not found"})
+    }
+
+    req.customer = customer
+
+    return next()
+}
+
+app.post("/account", (req, res) =>{
+    const {cpf, name} = req.body;
+
+    const customerAlreadyExists = customers.some((customer)=>customer.cpf === cpf)
+
+    if(customerAlreadyExists){
+        return res.status(400).json({error: "Customer Already Exists!"})
+    }
+
+    customers.push({
+        cpf, 
+        name, 
+        id: uuidv4(),
+        statement: []
+    })
+
+    return res.status(201).send()
+
+})
+
+app.get("/statement/", verifyIfExistsAccountCPF, (req, res)=>{
+    const { customer } = req
+    return res.json(customer.statement)
+})
+
+app.post("/deposit", verifyIfExistsAccountCPF, (req, res)=>{
+    const {description, amount} = req.body
+    const {customer} = req
+    const statementOperation = {
+        description, 
+        amount,
+        create_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation)
+
+    return res.status(201).send()
+})
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (req, res) =>{
+    const {customer} = req
+    const {date} = req.query
+
+    const dateFormate = new Date(date + " 00:00")
+
+    const statement = customer.statement.filter((statement)=>statement.create_at.toDateString() === new Date(dateFormate).toDateString())
+
+    return res.json(customer.statement)
+})
+
+app.put("/account", verifyIfExistsAccountCPF , (req, res) =>{
+    const {name} = req.body
+    const {customer} = req
+    customer.name = name
+
+    return res.status(201).send()
+
+})
+
+app.get("/account", verifyIfExistsAccountCPF , (req, res)=>{
+    const {customer} = req
+
+    return res.json(customer)
+})
+
+app.delete("/account", verifyIfExistsAccountCPF, (req, res)=>{
+    const {customer} = req
+    customers.splice(customer, 1)
+
+    return res.status(200).json(customers)
+})
+
+app.listen(3333)
